b7d868ae647313a0fca943f233926234c73f5fe4
Raimo Haikari <raimo.haikari@gmail.com>
2023-01-06 21:49:06 +0200

    Trip seederia paivitetty

--- database/seeders/TripSeeder.php
+++ database/seeders/TripSeeder.php
@@ -7,12 +7,65 @@ use App\Models\Trip;
 
 class TripSeeder extends Seeder
 {
+
+    /*
+     * departureStationID
+     * returnStationId
+     * coveredDistance
+     * duration
+     * 
+     * php artisan db:seed --class=DatabaseSeeder
+     */
+    public function run() {
+        // Tyhjennetään taulu
+        Trip::truncate();
+
+        $csvFile = fopen(base_path("database/data/2021-05-Prod.csv"), "r");
+        $firstline = true;
+
+        $temp_count = 0;
+        $values = [];
+
+        while (($data = fgetcsv($csvFile, 2000, ",")) !== FALSE) {
+            if (!$firstline) {
+
+                $temp_count++;
+
+                array_push(
+                    $values,
+                    [
+                        "departureStationID" => $data['0'],
+                        "returnStationId" => $data['1'],
+                        "coveredDistance" => $data['2'],
+                        "duration" => $data['3']
+                    ]
+                );
+
+                if ($temp_count % 1000 == 0) {
+
+                    Trip::insert($values);
+
+                    $values = [];
+                }
+
+   
+            }
+            $firstline = false;
+
+        }
+
+        if(count($values) > 0) {
+            Trip::insert($values);
+        }
+
+    }
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
+    public function fun()
     {
         // Tyhjennetään taulu
         Trip::truncate();
