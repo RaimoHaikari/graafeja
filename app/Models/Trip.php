bafb3e29e00d8bfdc1bc46b91dc56920b9bd9114
Raimo Haikari <raimo.haikari@gmail.com>
2023-01-06 22:41:14 +0200

    Testataan Trip seederia toukokuun tiedoilla

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -6,10 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trip extends Model
 {
+    /*
+     * departureStationId
+     * returnStationId
+     * coveredDistance
+     * duration
+     * dep_H
+     * dep_M
+     * dep_Day
+     * dep_Weekday
+     * dep_Month
+     * dep_Year
+     * ret_H
+     * ret_M
+     * ret_Day
+     * ret_Weekday
+     * ret_Month
+     * ret_Year
+     */
     protected $fillable = [
         'departureStationID', 
         'returnStationId', 
         'coveredDistance', 
         'duration',
+        'duration',
+        'dep_H',
+        'dep_M',
+        'dep_Day',
+        'dep_Weekday',
+        'dep_Month',
+        'dep_Year',
+        'ret_H',
+        'ret_M',
+        'ret_Day',
+        'ret_Weekday',
+        'ret_Month',
+        'ret_Year'
     ];
 }
--- database/seeders/TripSeeder.php
+++ database/seeders/TripSeeder.php
@@ -31,13 +31,43 @@ class TripSeeder extends Seeder
 
                 $temp_count++;
 
+                /*
+                 * departureStationId
+                 * returnStationId
+                 * coveredDistance
+                 * duration
+                 * dep_H
+                 * dep_M
+                 * dep_Day
+                 * dep_Weekday
+                 * dep_Month
+                 * dep_Year
+                 * ret_H
+                 * ret_M
+                 * ret_Day
+                 * ret_Weekday
+                 * ret_Month
+                 * ret_Year
+                 */
                 array_push(
                     $values,
                     [
                         "departureStationID" => $data['0'],
                         "returnStationId" => $data['1'],
                         "coveredDistance" => $data['2'],
-                        "duration" => $data['3']
+                        "duration" => $data['3'],
+                        "dep_H" => $data['4'],
+                        "dep_M" => $data['5'],
+                        "dep_Day" => $data['6'],
+                        "dep_Weekday" => $data['7'],
+                        "dep_Month" => $data['8'],
+                        "dep_Year" => $data['9'],
+                        "ret_H" => $data['10'],
+                        "ret_M" => $data['11'],
+                        "ret_Day" => $data['12'],
+                        "ret_Weekday" => $data['13'],
+                        "ret_Month" => $data['14'],
+                        "ret_Year" => $data['15'],
                     ]
                 );
 
