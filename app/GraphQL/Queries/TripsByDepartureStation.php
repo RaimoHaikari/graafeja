fa902437713271dc8a320f1717767c3c27104282
Raimo Haikari <raimo.haikari@gmail.com>
2023-01-08 17:45:02 +0200

    Asemalta tehdyt lainat - GraphQL

---
+++ app/GraphQL/Queries/TripsByDepartureStation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\GraphQL\Queries;
+use Illuminate\Support\Facades\DB;
+use App\Models\TripsByDepartureStation  AS TripsByDepartureStationModel;
+
+final class TripsByDepartureStation
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $val= array();
+
+        $query = <<<END
+        SELECT *
+        FROM tripsbydeparturestation
+      END;
+
+        $data = DB::select($query);
+
+        
+        foreach ($data as $d) {
+            array_push(
+                $val, 
+                new TripsByDepartureStationModel(
+                    [
+                        'departureStationID' => $d->departureStationID,
+                        'lkm' =>  $d->lkm,
+                        'maxDistance' => $d->maxDistance,
+                        'avgDistance' => $d->avgDistance,
+                        'minDistance' => $d->minDistance,
+                        'maxDuration' =>  $d->maxDuration,
+                        'avgDuration' => $d->avgDuration,
+                        'minDuration' => $d->minDuration,
+                    ]
+                )
+            );
+        }
+        
+        return $val;
+    }
+}
