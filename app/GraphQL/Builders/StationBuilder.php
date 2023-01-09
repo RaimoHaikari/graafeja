8fc35ac7daa0f1566ac751c53241a0ab2b6dba43
Raimo Haikari <raimo.haikari@gmail.com>
2023-01-09 22:01:48 +0200

    Query: stations paginate korjattu / GraphQl

---
+++ app/GraphQL/Builders/StationBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GraphQL\Builders;
+
+use App\Models\Station;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Log;
+
+class StationBuilder {
+
+    public function findStations($root, array $args): Builder
+    {
+
+        Log::info(json_encode($args));
+        // $stations = Station::where('nimi', 'LIKE', $searchStr.'%')->orderBy($orderBy, $order)->get();
+
+        return Station::query()
+            ->when($args['searchStr'], function($query, $searchStr){
+                $query
+                    ->where('nimi', 'LIKE', "$searchStr%");
+            });
+    }
+}
